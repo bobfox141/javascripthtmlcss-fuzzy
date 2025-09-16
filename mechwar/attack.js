864fecbcbb8f332c539f0b571c4759dcda751ff4
Steven Garbeil <bobfox141@gmail.com>
2025-09-16 16:10:57 -0500

    Split out the attacks and the base mech class to make it easier to test.

---
+++ mechwar/attack.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// the standard attack class. This is really simple for now, but it will grow.
+// an attack is an instance in itself because it doesn't matter to the target
+// what hit it.
+
+class Attack {
+  constructor(c, b, d) {
+    this.hit = true; // seems obvious but...
+    this.critical = c;
+    this.badmiss = b;
+    this.damage = d; // damage is transferred from attack to defense...
+    this.damagebonus = 2;
+  }
+}
